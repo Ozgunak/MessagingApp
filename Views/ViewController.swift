e720129ecc845c77d49adbdd15db55217fa3c3a1
Özgün Aksoy <64470656+Ozgunak@users.noreply.github.com>
2022-01-10 17:49:07 +0300

    Initial Commit

---
+++ Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Views
+//
+//  Created by ozgun on 10.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
