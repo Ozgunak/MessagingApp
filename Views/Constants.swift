64a2e3e86126d5cedd7ac6ced4ee777ec7ace01e
Özgün Aksoy <64470656+Ozgunak@users.noreply.github.com>
2022-01-13 22:42:39 +0300

    Added login and register. Added tableView cells with custom xib. Login and sending content availible with Firebase.

---
+++ Views/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Views
+//
+//  Created by ozgun on 13.01.2022.
+//
+
+struct K {
+    static let mainSegue = "GoToMain"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "ContentCell"
+    static let collectionName = "Content"
+    static let senderField = "sender"
+    static let bodyField = "body"
+    static let dateField = "date"
+}
---
+++ Views/Model/Content.swift
@@ -0,0 +1,13 @@
+//
+//  Content.swift
+//  Views
+//
+//  Created by ozgun on 13.01.2022.
+//
+
+import Foundation
+
+struct Content {
+    let sender: String
+    let contentBody: String
+}
